1e8515acbe5fa8992008ef8f5081c4dcbb80f0a3
Tomek Felisiak <tomekfel@googlemail.com>
2021-04-13 20:41:37 +0100

    episode list complete

--- screens/MovieDetailsScreen/index.tsx
+++ screens/MovieDetailsScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Image,
@@ -6,19 +6,19 @@ import {
   ImageBackground,
   Pressable,
   FlatList,
-} from 'react-native';
-import { SharedElement } from 'react-navigation-shared-element';
+} from "react-native";
+import { SharedElement } from "react-navigation-shared-element";
 import {
   MaterialIcons,
   Entypo,
   AntDesign,
   Feather,
   FontAwesome,
-} from '@expo/vector-icons';
+} from "@expo/vector-icons";
 
-import styles from './styles';
-import { data } from '../../config/data';
-import EpisodeItem from '../../components/EpisodeItem';
+import styles from "./styles";
+import { data } from "../../config/data";
+import EpisodeItem from "../../components/EpisodeItem";
 
 const MovieDetailsScreen = ({
   route,
@@ -29,16 +29,90 @@ const MovieDetailsScreen = ({
 }) => {
   const { item } = route.params;
 
-  // console.log(item);
+  const renderedList = (
+    <View style={styles.details}>
+      <View style={styles.titleBackground}>
+        <Text style={styles.title}>{item.imdb_title}</Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            // justifyContent: 'center',
+            alignItems: "center",
+          }}
+        >
+          <Text style={styles.match}>{item.imdb_user_rating}</Text>
+          <Text style={styles.year}>{parseInt(item.imdb_date_published)}</Text>
+          <View style={styles.ageContainer}>
+            <Text style={styles.age}>{item.imdb_rating}</Text>
+          </View>
+          <Text style={styles.year}>{item.imdb_duration}</Text>
+          {/* <MaterialIcons name='hd' size={24} color='white' /> */}
+        </View>
+      </View>
+
+      {/* Buttons */}
+      <Pressable onPress={() => console.log("Play")} style={styles.playButton}>
+        <Text style={styles.playButtonText}>
+          <Entypo name="controller-play" size={16} color="black" /> Play
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={() => console.log("Download")}
+        style={styles.downloadButton}
+      >
+        <Text style={styles.downloadButtonText}>
+          <AntDesign name="download" size={16} color="white" /> Download
+        </Text>
+      </Pressable>
+
+      {/* Description */}
+      <View>
+        <Text style={styles.description}>{item.imdb_description}</Text>
+        <Text style={styles.genre}>{item.imdb_genre}</Text>
+
+        {/* Row with icon buttons */}
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={styles.icon}>
+            <AntDesign name="plus" size={24} color="white" />
+            <Text style={styles.iconText}>My List</Text>
+          </View>
+          <View style={styles.icon}>
+            <Feather name="thumbs-up" size={24} color="white" />
+            <Text style={styles.iconText}>Rate</Text>
+          </View>
+          <View style={styles.icon}>
+            <FontAwesome name="send-o" size={24} color="white" />
+            <Text style={styles.iconText}>Share</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
+  const renderItem = ({ item }: { item: any }) => (
+    <Pressable
+      key={item.imdb_link}
+      onPress={() =>
+        navigation.push("DetailsScreen", {
+          item,
+        })
+      }
+    >
+      <EpisodeItem episode={item} />
+    </Pressable>
+  );
+
   return (
     <View style={styles.container}>
+      {/* Image */}
       <ImageBackground
         source={
           item.imdb_image_url
             ? {
                 uri: item.imdb_image_url,
               }
-            : require('../../assets/images/no-image.png')
+            : require("../../assets/images/no-image.png")
         }
         style={styles.backgroundImage}
         blurRadius={10}
@@ -51,77 +125,18 @@ const MovieDetailsScreen = ({
                 ? {
                     uri: item.imdb_image_url,
                   }
-                : require('../../assets/images/no-image.png')
+                : require("../../assets/images/no-image.png")
             }
           />
         </SharedElement>
       </ImageBackground>
 
-      <View style={styles.details}>
-        <View style={styles.titleBackground}>
-          <Text style={styles.title}>{item.imdb_title}</Text>
-
-          <View
-            style={{
-              flexDirection: 'row',
-              // justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={styles.match}>{item.imdb_user_rating}</Text>
-            <Text style={styles.year}>
-              {parseInt(item.imdb_date_published)}
-            </Text>
-            <View style={styles.ageContainer}>
-              <Text style={styles.age}>{item.imdb_rating}</Text>
-            </View>
-            <Text style={styles.year}>{item.imdb_duration}</Text>
-            {/* <MaterialIcons name='hd' size={24} color='white' /> */}
-          </View>
-        </View>
-      </View>
-
-      {/* Buttons */}
-      <Pressable onPress={() => console.log('Play')} style={styles.playButton}>
-        <Text style={styles.playButtonText}>
-          <Entypo name='controller-play' size={16} color='black' /> Play
-        </Text>
-      </Pressable>
-      <Pressable
-        onPress={() => console.log('Download')}
-        style={styles.downloadButton}
-      >
-        <Text style={styles.downloadButtonText}>
-          <AntDesign name='download' size={16} color='white' /> Download
-        </Text>
-      </Pressable>
-
-      {/* Description */}
-      <View>
-        <Text style={styles.description}>{item.imdb_description}</Text>
-        <Text style={styles.genre}>{item.imdb_genre}</Text>
-
-        {/* Row with icon buttons */}
-        <View style={{ flexDirection: 'row', marginTop: 20 }}>
-          <View style={styles.icon}>
-            <AntDesign name='plus' size={24} color='white' />
-            <Text style={styles.iconText}>My List</Text>
-          </View>
-          <View style={styles.icon}>
-            <Feather name='thumbs-up' size={24} color='white' />
-            <Text style={styles.iconText}>Rate</Text>
-          </View>
-          <View style={styles.icon}>
-            <FontAwesome name='send-o' size={24} color='white' />
-            <Text style={styles.iconText}>Share</Text>
-          </View>
-        </View>
-      </View>
-
       {/* Episodes */}
       <FlatList
         data={data.records}
-        renderItem={({ item }) => <EpisodeItem episode={item} />}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.imdb_link}
+        ListHeaderComponent={renderedList}
       />
     </View>
   );
@@ -132,8 +147,8 @@ MovieDetailsScreen.sharedElements = (route: any) => {
   return [
     {
       id: `item.${item.imdb_image_url}.image_url`,
-      animation: 'move',
-      resize: 'clip',
+      animation: "move",
+      resize: "clip",
     },
   ];
 };
